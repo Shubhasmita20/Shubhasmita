3b2cdb316467bfb17ee13e5ad2cc759b9dde3af7
Shubhasmita <149405218+Shubhasmita20@users.noreply.github.com>
2024-06-27 14:02:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menue-link")
+    const icon = document.querySelector(".hamburger-icon")
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
